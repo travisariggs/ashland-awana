5cf5afc49c25070173e86c02b4eeceea5af7647b
Travis Riggs <travis.a.riggs@gmail.com>
2017-05-27 21:22:31 -0400

    Made models editable in admin interface

--- verses/admin.py
+++ verses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Student, Verse, Recitation
+
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Verse)
+admin.site.register(Recitation)
